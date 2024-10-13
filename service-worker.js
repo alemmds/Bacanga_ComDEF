4a2378e572ce7d295f91e3ece589d51855e2111f
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-13 19:36:04 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,50 @@
-const CACHE_NAME = 'panificadora-cache-v1';
+const CACHE_NAME = 'panificadora-bacanga-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/favicon-48x48.png',
-  '/favicon.svg',
-  '/favicon.ico',
-  '/apple-touch-icon.png',
-  '/logo.png' // Adicione a logo ao cache
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/logo.png',
+    '/icon.png',
+    '/icon-large.png',
+    '/manifest.json',
+    '/favicon-48x48.png',
+    '/favicon.svg',
+    '/favicon.ico',
+    '/apple-touch-icon.png'
 ];
 
-// Instalando o Service Worker e fazendo cache dos arquivos
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+// Instalação do Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Arquivos em cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-// Buscando arquivos do cache
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
+// Ativação do Service Worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Interceptação de requisições e busca no cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
